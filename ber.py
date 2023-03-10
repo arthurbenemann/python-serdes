60d565cc33798f2968196d9ddf30187209fc5934
arthurbenemann <arthur.benemann@gmail.com>
2023-03-09 18:14:10 -0800

    split out each sim, add rate in tqdm

--- ber.py
+++ ber.py
@@ -17,6 +17,13 @@ def add_noise(signal, snr):
     noisy_signal = signal + noise
     return noisy_signal
 
+def channel_sim(signal, channel, snr):
+    noise_power = 10 ** (snr / 10)
+    noisy_signal = add_noise(signal, noise_power)                       
+    isi_signal = simulate_isi(signal,channel)
+    received_signal = add_noise(isi_signal,noise_power)
+    return received_signal
+
 def calculate_ber(original_bits, decoded_bits):
     errors = np.sum(original_bits != decoded_bits)
     ber = errors / len(original_bits)
@@ -73,8 +80,9 @@ def dfe(signal, taps):
 
 def main():
     # Simulation paramenters
-    N = int(1e6)  # number of bits
-    snr_range = np.linspace(0, 10, 6)  # range of SNR values
+    N = int(1e5)  # number of bits
+    steps = 10
+    snr_range = np.linspace(0, 10, steps)  # range of SNR values
 
     ber_raw = np.zeros(len(snr_range))
     ber_isi = np.zeros(len(snr_range))
@@ -85,29 +93,29 @@ def main():
     np.random.seed(0)   # lock seed for repeatable results 
     bits = np.random.randint(0, 2, N)  # generate random bits
     signal = nrz_encode(bits)  # encode the bits using NRZ
+    channel = np.array([1.,.9,.5,0.2,-0.15])
 
+    
+    for i in tqdm(range(len(snr_range)), desc='ideal',unit_scale=N//1e3, unit='kbit'):
+        received_signal = channel_sim(signal, np.ones(1), snr_range[i])
+        ber_raw[i] = calculate_ber(bits, nrz_decode(received_signal) )  
+    
+    for i in tqdm(range(len(snr_range)), desc='isi',unit_scale=N//1e3, unit='kbit'):
+        received_signal = channel_sim(signal, channel, snr_range[i])
+        ber_isi[i] = calculate_ber(bits, nrz_decode(received_signal)  )
 
-    # SNR simulations
-    for i in tqdm(range(len(snr_range))):
+    for i in tqdm(range(len(snr_range)), desc='ffe',unit_scale=N//1e3, unit='kbit'):
+        received_signal = channel_sim(signal, channel, snr_range[i])
+        ffe_signal = ffe(received_signal, [1,-.5,+.05,+.08])
+        ber_ffe[i] = calculate_ber(bits, nrz_decode(ffe_signal))  
 
-        # Channel simultation
-        noise_power = 10 ** (snr_range[i] / 10)
-        noisy_signal = add_noise(signal, noise_power)                       
-        isi_signal = simulate_isi(signal,np.array([1.,.5,.2,0,-0.15]))
-        noisy_isi_signal = add_noise(isi_signal,noise_power)
-
-        # Equalization, 
-        ffe_signal = ffe(noisy_isi_signal, [1,-.5,+.05,+.08])
-        dfe_signal = dfe(noisy_isi_signal,[.5,.2,0,-.15])
-
-        # Detectors and BER calculation
-        ber_raw[i] = calculate_ber(bits, nrz_decode(noisy_signal) )  # calculate BER for current SNR
-        ber_isi[i] = calculate_ber(bits, nrz_decode(noisy_isi_signal)  )  # calculate BER for current SNR
-        ber_ffe[i] = calculate_ber(bits, nrz_decode(ffe_signal))  # calculate BER for current SNR
-        ber_dfe[i] = calculate_ber(bits, nrz_decode(dfe_signal))  # calculate BER for current SNR
+    for i in tqdm(range(len(snr_range)), desc='dfe',unit_scale=N//1e3, unit='kbit'):
+        received_signal = channel_sim(signal, channel, snr_range[i])
+        dfe_signal = dfe(received_signal,[.9,.5,0.2,-0.15])
+        ber_dfe[i] = calculate_ber(bits, nrz_decode(dfe_signal))  
 
     # Plotting
-    plt.semilogy(snr_range, ber_raw, label='raw')
+    plt.semilogy(snr_range, ber_raw, label='ideal')
     plt.semilogy(snr_range, ber_dfe, label='dfe-4tap')
     plt.semilogy(snr_range, ber_ffe, label='ffe-4tap')
     plt.semilogy(snr_range, ber_isi, label='isi')
@@ -117,6 +125,6 @@ def main():
     plt.grid(which='major',  linestyle='-')
     plt.grid(which='minor', color='dimgrey', linestyle='--')
     plt.legend()
-    plt.show()
+    plt.savefig('snr_vs_ber.png')
 
-print(timeit.timeit(main, number=1))
+main()
