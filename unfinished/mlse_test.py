119f9117fdfcdc55d0f9a70cc783b6310bcefa7c
arthurbenemann <arthur.benemann@gmail.com>
2023-03-12 11:44:39 -0700

    Add noise and longer channel

--- unfinished/mlse_test.py
+++ unfinished/mlse_test.py
@@ -75,8 +75,9 @@ np.set_printoptions(precision=1, suppress=True,
 np.random.seed(0)   # lock seed for repeatable results
 o = np.where(np.random.randint(0, 2, 100) == 1, 1, -1)  # generate random bits
 # o = np.array([1, 1,  1, 1, 1, 1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-h = np.array([1, 0.8, 0.2])
+h = np.array([1, -1,2,1,3,2,0.1])
 r2 = np.convolve(o, h)
+r2 += np.random.normal(0, 1.0, len(r2))  # noise
 
 traceback = 10
 detections = mlse(r2, h, traceback)
