e2643e4eacfc48faa106b5e4832c9dd48350b8f4
arthurbenemann <arthur.benemann@gmail.com>
2023-03-11 19:42:00 -0800

    commiting progress thus far, not functional

--- unfinished/mlse_test.py
+++ unfinished/mlse_test.py
@@ -1,45 +1,101 @@
+# How I learned to love the trellis - Signal Processing Magazine, IEEE
+# https://www.ece.ucdavis.edu/~bbaas/281/notes/Handout.viterbi.pdf
+
 import numpy as np
+from itertools import product
 
 
 def mlse(signal, channel_response, traceback_length):
 
-    K = 2**len(channel_response)   # number of states
+    channel_length = len(channel_response)
 
-    branch_metrics = fun(channel_response)  # needs function, size 2 * K
+    # Generate all possible transmitted sequences
+    m = np.array(list(product([-1, 1], repeat=3))).reshape((8, 3))
+
+    # branch metrics considering n,n-1,n-2 indexing to messages
+    decoder_trellis = np.dot(m, channel_response)
+
+    # transition matrix
+    transition_matrix = np.array([[0, 1], [2, 3], [0, 1], [2, 3]])
+
+    K = 2**(len(channel_response)-1)   # number of states
 
     # Γ same as the number of states = len(channel)
     state_metrics = np.zeros(K)
 
-    path_history = np.zeros((K, traceback_length))  # each row is a path ?
+    # each row is a path ?
+    path_mem = np.zeros((K, traceback_length), dtype=bool)
 
-    detections = np.zeros_like(signal)
+    # detected signal
+    detections = []
 
     # run through samples
-    for n in range(signal):
+    for sample in signal:
 
-        # Add Compare Select
-        for j in range(state_metrics):
+        # double the vector by replicating end-to-end
+        previous_state_metrics = np.tile(state_metrics, 2)
 
-            # need to figure out mapping
-            one = np.abs(signal[n] - state_metrics[x] + j)
-            zero = np.abs(signal[n] - state_metrics[y] + j)
+        # Calculate Branch metrics
+        branch_metrics = np.abs(decoder_trellis - sample)
 
-            if (one < zero):
-                state_metrics[z] = state_metrics[x]
-                path_history[z].append(1)
-            else:
-                state_metrics[z] = state_metrics[y]
-                path_history[z].append(0)
+        # Add
+        metric = previous_state_metrics + branch_metrics
 
-        # select path
-        detections[n] = path_history[np.argmin(state_metrics)]
-        shift_path_history()
+        # Compare
+        even_metrics = metric[::2]
+        odd_metrics = metric[1::2]
+        decisions = even_metrics < odd_metrics
 
+        # Select
+        state_metrics = np.where(
+            decisions, even_metrics, odd_metrics)  # select lower metric
+
+        # Update path memmory
+        path_mem = np.roll(path_mem, 1)
+        path_mem[:, 0] = decisions
+
+        # Find most likely path
+        likely_state = np.argmin(state_metrics)
+
+        for decisions in path_mem:
+            traceback_decision = int(decisions[likely_state])
+            likely_state = transition_matrix[likely_state, traceback_decision]
+
+        # decide bit based on oldest state on likely path
+        bit = likely_state > K/2
+        detections =np.append(detections, bit)
+        # print(path_mem, lowblue(state_metrics),   lowblue(branch_metrics), bit)
     return detections
 
 
-    # path memmory
-r = np.array([1., 1, 1, -1, -1, -1, 1, 1, 1, -1, -1, 1, 1, 1, 1])
-h = np.array([1, 0.8, 0.5, 0.2])
+def lowblue(vector):
+    # format the numbers in the vector with 1 decimal place and leading zeros
+    formatted_vector = [f"{num:0>.1f}" for num in vector]
 
-mlse(r, h)
+    # find the index of the lowest value
+    min_index = np.argmin(vector)
+
+    # create a string with ANSI escape codes to print in blue
+    blue_text = "\033[34m{}\033[0m".format(formatted_vector[min_index])
+
+    # replace the lowest value in the vector with the blue text
+    formatted_vector[min_index] = blue_text
+
+    # join the formatted numbers into a string and return it
+    return "[" + " ".join(formatted_vector) + "]"
+
+
+np.set_printoptions(precision=1, suppress=True, floatmode='fixed')
+
+# path memmory
+
+
+o = np.array([1, 1,  1, 1, 1, 1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
+h = np.array([1, 0.8, 0.2])
+r2 = np.convolve(o, h)
+
+detections = mlse(r2, h, 4)
+
+
+print((o > 0).astype(int))
+print(detections.astype(int))
