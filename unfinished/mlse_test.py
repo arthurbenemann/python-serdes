b27133c8157d01dc0959af448feec97b7298ca2f
arthurbenemann <arthur.benemann@gmail.com>
2023-03-12 09:56:45 -0700

    fix bugs, working for traceback=4

--- unfinished/mlse_test.py
+++ unfinished/mlse_test.py
@@ -24,7 +24,7 @@ def mlse(signal, channel_response, traceback_length):
     state_metrics = np.zeros(K)
 
     # each row is a path ?
-    path_mem = np.zeros((K, traceback_length), dtype=bool)
+    path_mem = np.zeros((traceback_length, K), dtype=bool)
 
     # detected signal
     detections = []
@@ -44,27 +44,32 @@ def mlse(signal, channel_response, traceback_length):
         # Compare
         even_metrics = metric[::2]
         odd_metrics = metric[1::2]
-        decisions = even_metrics < odd_metrics
+        decisions = odd_metrics < even_metrics
 
         # Select
         state_metrics = np.where(
-            decisions, even_metrics, odd_metrics)  # select lower metric
+            decisions, odd_metrics, even_metrics)  # select lower metric
 
         # Update path memmory
-        path_mem = np.roll(path_mem, 1)
-        path_mem[:, 0] = decisions
+        path_mem = np.roll(path_mem, 1, axis=0)
+        path_mem[0, :] = decisions
 
         # Find most likely path
         likely_state = np.argmin(state_metrics)
 
-        for decisions in path_mem:
-            traceback_decision = int(decisions[likely_state])
+        for test in path_mem:
+            traceback_decision = int(test[likely_state])
             likely_state = transition_matrix[likely_state, traceback_decision]
 
         # decide bit based on oldest state on likely path
-        bit = likely_state > K/2
-        detections =np.append(detections, bit)
-        # print(path_mem, lowblue(state_metrics),   lowblue(branch_metrics), bit)
+        bit = likely_state >= K/2
+        detections = np.append(detections, bit)
+
+        # debug
+        print(lowblue(np.transpose(state_metrics)),
+              lowblue(branch_metrics), bit)
+        print(path_mem)
+        print()
     return detections
 
 
@@ -85,17 +90,25 @@ def lowblue(vector):
     return "[" + " ".join(formatted_vector) + "]"
 
 
-np.set_printoptions(precision=1, suppress=True, floatmode='fixed')
+np.set_printoptions(precision=1, suppress=True,
+                    floatmode='fixed', linewidth=250)
 
 # path memmory
 
 
-o = np.array([1, 1,  1, 1, 1, 1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
+np.random.seed(0)   # lock seed for repeatable results
+o = np.where(np.random.randint(0, 2, 100) == 1, 1, -1)  # generate random bits
+# o = np.array([1, 1,  1, 1, 1, 1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
 h = np.array([1, 0.8, 0.2])
 r2 = np.convolve(o, h)
 
 detections = mlse(r2, h, 4)
 
 
-print((o > 0).astype(int))
-print(detections.astype(int))
+transmitted = (o > 0).astype(int)
+received = detections[4:].astype(int)
+errors = (received != transmitted[:-2]).astype(int)
+
+print(transmitted)
+print(received)
+print(errors, '\t errors:', np.sum(errors))
